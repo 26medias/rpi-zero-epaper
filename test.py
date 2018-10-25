b19258f0489f00440d267499ed7e0fdba0d19d3d
Twenty-Six medias, Inc <julien@twenty-six-medias.com>
2018-10-24 23:17:34 -0400

    test

--- test.py
+++ test.py
@@ -34,6 +34,29 @@ def main():
     epd = epd2in9.EPD()
     epd.init(epd.lut_full_update)
 
+    # For simplicity, the arguments are explicit numerical coordinates
+    image = Image.new('1', (epd2in9.EPD_WIDTH, epd2in9.EPD_HEIGHT), 255)  # 255: clear the frame
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('open-sans.ttf', 16)
+    draw.rectangle((0, 10, 128, 34), fill = 0)
+    draw.text((8, 12), 'Hello world!', font = font, fill = 255)
+    draw.text((8, 36), 'e-Paper Demo', font = font, fill = 0)
+    draw.line((16, 60, 56, 60), fill = 0)
+    draw.line((56, 60, 56, 110), fill = 0)
+    draw.line((16, 110, 56, 110), fill = 0)
+    draw.line((16, 110, 16, 60), fill = 0)
+    draw.line((16, 60, 56, 110), fill = 0)
+    draw.line((56, 60, 16, 110), fill = 0)
+    draw.arc((60, 90, 120, 150), 0, 360, fill = 0)
+    draw.rectangle((16, 130, 56, 180), fill = 0)
+    draw.chord((60, 160, 120, 220), 0, 360, fill = 0)
+    
+    epd.clear_frame_memory(0xFF)
+    epd.set_frame_memory(image, 0, 0)
+    epd.display_frame()
+
+    epd.delay_ms(2000)
+
 ##
  # there are 2 memory areas embedded in the e-paper display
  # and once the display is refreshed, the memory area will be auto-toggled,
@@ -44,31 +67,6 @@ def main():
     epd.display_frame()
     epd.clear_frame_memory(0xFF)
     epd.display_frame()
-
-    # for partial update
-    epd.init(epd.lut_partial_update)
-    image = Image.open('monocolor.bmp')
-##
- # there are 2 memory areas embedded in the e-paper display
- # and once the display is refreshed, the memory area will be auto-toggled,
- # i.e. the next action of SetFrameMemory will set the other memory area
- # therefore you have to set the frame memory twice.
- ##     
-    epd.set_frame_memory(image, 0, 0)
-    epd.display_frame()
-    epd.set_frame_memory(image, 0, 0)
-    epd.display_frame()
-
-    time_image = Image.new('1', (96, 32), 255)  # 255: clear the frame
-    draw = ImageDraw.Draw(time_image)
-    font = ImageFont.truetype('open-sans.ttf', 12)
-    image_width, image_height = time_image.size
-    while (True):
-        # draw a rectangle to clear the image
-        draw.rectangle((0, 0, image_width, image_height), fill = 255)
-        draw.text((0, 0), time.strftime('%M:%S'), font = font, fill = 0)
-        epd.set_frame_memory(time_image.rotate(270), 80, 80)
-        epd.display_frame()
 
 if __name__ == '__main__':
     main()
